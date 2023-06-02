ad1fb968a3754fa908b186d064355c8cac4dad02
Joaquín <joaquincarrozza@gmail.com>
2023-06-02 13:18:41 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
-#include <cmath> /*biblioteca para potencia*/
+#include<iostream>
+#include<cmath> /*biblioteca para pow*/
+#include<iomanip>
 using namespace std;
 
 int main() {
@@ -9,12 +10,12 @@ int main() {
         double termino = 4 * pow(-1, k) / (2 * k + 1); /*calculo de serie de leibniz*/
         pi += termino;
 
-        if (abs(pi - 3.141592) < 0.000001)  {
+        if (abs(pi - 3.1415926) < 0.000001)  {
             break;
-        } /*cuando pi llegue a 6 decimales, dejar de iterar*/
+        } /*cuando pi llegue a 6 decimales, dejar re iterar*/
     }
 
-    cout << pi << endl;
+    cout << setprecision(7) << pi << endl;
 
     return 0;
 }
