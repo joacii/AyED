c6b9b5e37f6bc5bf0b782654c5dc3c778d254662
Joaquín <joaquincarrozza@gmail.com>
2023-06-02 00:45:11 -0300

    Add files via upload

---
+++ 01-CalcularPi/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath> /*biblioteca para pow*/
+using namespace std;
+
+int main() {
+    double pi = 0;
+
+    for (int k = 0; ; k++) {
+        double termino = 4 * pow(-1, k) / (2 * k + 1); /*calculo de serie de leibniz*/
+        pi += termino;
+
+        if (abs(pi - 3.141592) < 0.000001)  {
+            break;
+        } /*cuando pi llegue a 6 decimales, dejar re iterar*/
+    }
+
+    cout << pi << endl;
+
+    return 0;
+}
